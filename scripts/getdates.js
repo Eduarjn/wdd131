0c606b93cfd6d1d9a8eb994ee83e16c954fc6706
Eduarjn <eduarjose.fajardo96@gmail.com>
2026-08-31 20:16:11 -0300

    Add dynamic footer data for current year and last modified

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// W01 - Dynamic footer data
+const yearSpan = document.querySelector("#currentyear");
+const lastModifiedParagraph = document.querySelector("#lastModified");
+
+yearSpan.textContent = new Date().getFullYear();
+lastModifiedParagraph.textContent = "Last Modification: " + document.lastModified;
